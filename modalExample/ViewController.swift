333823d9a87fbd07390c5c0e73e98d5ca28b08ef
Hector De Diego Brito <hector.dediego.brito@gmail.com>
2018-07-25 11:53:56 -0500

    Initial Commit

---
+++ modalExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  modalExample
+//
+//  Created by Hector de Diego on 25/07/18.
+//  Copyright © 2018 Hector de Diego. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
